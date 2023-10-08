b73a38bfcf058ac9257dc10bd993afe23f934aa7
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-10-08 20:17:29 +0200

    added lbp and hsv features, retrain sample

--- helpers/feature_extraction.py
+++ helpers/feature_extraction.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import mode, skew, kurtosis, entropy
 
 from skimage.feature import graycomatrix, graycoprops
+from skimage.feature import local_binary_pattern
 
 class FeatureExtraction:
     def __init__(self) -> None:
@@ -33,10 +34,24 @@ class FeatureExtraction:
         hist_g = (cv2.calcHist([g], [0], None, [n_bins], [0, 256])) / N
         hist_b = (cv2.calcHist([b], [0], None, [n_bins], [0, 256])) / N
 
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv_img)
+
+        # Calculate normalized histograms for each channel
+        hist_h = (cv2.calcHist([h], [0], None, [n_bins], [0, 256])) / N
+        hist_s = (cv2.calcHist([s], [0], None, [n_bins], [0, 256])) / N
+        hist_v = (cv2.calcHist([v], [0], None, [n_bins], [0, 256])) / N
+
+
         color_features = np.concatenate((
             self.calculate_hist_features(hist_r), 
             self.calculate_hist_features(hist_g), 
-            self.calculate_hist_features(hist_b))
+            self.calculate_hist_features(hist_b),
+            
+            self.calculate_hist_features(hist_h), 
+            self.calculate_hist_features(hist_s), 
+            self.calculate_hist_features(hist_v)
+            )
         )
 
         return color_features
@@ -63,12 +78,48 @@ class FeatureExtraction:
         texture_features = np.concatenate([graycoprops(glcm, feature).ravel() for feature in features])
         
         return texture_features
+    
+    def extract_lbp_features(self, image):
+        ''' 
+        Extracts the histogram of the LBP image for a certain number of neighbours and radius.
+
+        Args:
+            image (numpy.ndarray): the image from which the LBP features will be extracted (cropped ROI)
+            P (int): number of neighbours
+            R (int): radius
+        Output:
+            hist (numpy.array): histogram of the LBP image
+        '''
+
+        P=[8, 16] 
+        R=[1, 2]
+        histogram = []
+
+        # Convert image to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        for idx, P_value in enumerate(P):
+
+            n_points = P_value * R[idx]
+
+            lbp_image = local_binary_pattern(gray_image, n_points, R[idx], method='uniform')
+
+            # Calculate the histogram of the LBP image
+            hist, _ = np.histogram(lbp_image.ravel(), bins=np.arange(0, n_points + 3), range=(0, n_points + 2), density=True)
+
+            # Normalize the histogram
+            hist /= np.sum(hist)
+
+            histogram = np.concatenate((histogram, hist))
+        
+        return histogram
         
     def fit(self, image):
         color_features      = self.extract_color_features(image)
         texture_features    = self.extract_glcm_features(image)
+        lbp_features        = self.extract_lbp_features(image)
 
-        all_features = np.concatenate((color_features, texture_features))
+        all_features = np.concatenate((color_features, texture_features, lbp_features))
 
         return all_features
 
